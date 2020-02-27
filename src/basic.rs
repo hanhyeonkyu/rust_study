3981e39ed391919650e3a7a6629ca702fa653a5c
hanhyeonkyu <busgod1212@gmail.com>
2020-02-27 10:58:36 +0900

    rust mutable vs immutable2

--- src/basic.rs
+++ src/basic.rs
@@ -2,9 +2,25 @@
 
 pub fn variables() {
     // mutable && immutable diff
+    // this is immutable
     let x = 5;
     // x = 3;
-    let mut y = 1;
-    y = 4;
+    // this is mutable
+    // let mut y = 1;
+    let y = 4;
     println!("x = {} && y = {}", x, y);
+
+    // immutable cal
+    let z = 5;
+    let z = z + 1;
+    let z = z * 2;
+    println!("z is {}", z);
+
+    // 1st case is str, 2nd case is number. so mutable value can't mutable data type.
+    let spaces = "          ";
+    let spaces = spaces.len();
+    // let mut spaces = "           ";
+    // let mut spaces = spaces.len();
+    println!("spaces length is {}", spaces);
+
 }
